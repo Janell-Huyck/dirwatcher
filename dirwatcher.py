29feea40461d72cef4c296a906e21370cc6021e2
Janell Huyck <Janell.Huyck@outlook.com>
2020-03-25 16:26:29 -0400

    made pipenv and added rudimentary logging for start and stop of program

--- dirwatcher.py
+++ dirwatcher.py
@@ -4,8 +4,10 @@
 __author__ = "Janell.Huyck"
 
 import sys
+import argparse
 # import os
-# import time
+import datetime
+import logging
 
 # Make sure that the user is running this program in python3
 
@@ -13,10 +15,49 @@ if sys.version_info[0] < 3:
     raise Exception("This program requires python 3 to work.")
     sys.exit(1)
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    format='%(asctime)s.%(msecs)03d %(name)-12s %(levelname)-8s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S')
+logger.setLevel(logging.DEBUG)
 
-def main():
-    print("main is running")
+
+def create_parser():
+    """ Create and return a parser object """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dir', help='Directory to watch')
+    parser.add_argument('ext', help="""File extension for type of
+                        file to watch in the directory""")
+    parser.add_argument('magic', help='The string we are watching for.')
+    parser.add_argument('-poll', default="1", help="""How often (in seconds) to
+                        poll the directory.  Default is 1 second.""")
+
+    return parser
+
+
+def main(args):
+    app_start_time = datetime.datetime.now()
+    logger.info('\n'
+                '=========================================================\n'
+                '\tRunning {}\n'
+                '\tStarted at: {}\n'
+                '=========================================================\n'
+                .format(__file__, str(app_start_time)))
+    parser = create_parser()
+    if not args:
+        parser.print_usage()
+        sys.exit(1)
+    parsed_args = parser.parse_args(args)
+    logger.debug("main is running")
+    uptime = datetime.datetime.now() - app_start_time
+    logger.info('\n'
+                '=========================================================\n'
+                '\tStopped {}\n'
+                '\tUptime was: {}\n'
+                '=========================================================\n'
+                .format(__file__, str(uptime)))
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
--- tests/test_dirwatcher.py
+++ tests/test_dirwatcher.py
@@ -17,11 +17,22 @@ __author__ = "Janell.Huyck, based off madarp's test code for babynames"
 
 class TestDirwatcher(unittest.TestCase):
     def test_create_parser(self):
-        """Check if parser can parse args"""
+        """Check if parser can parse args correctly"""
         p = dirwatcher.create_parser()
-        test_args = ['watchdir', 'txt', '-poll 3', 'Tiffany']
+        test_args = ['watchdir', 'txt', '-poll', '3', 'Tiffany']
         ns = p.parse_args(test_args)
-        self.assertTrue(ns.dir == 'watchdir')
-        self.assertTrue(ns.ext == 'txt')
-        self.assertTrue(ns.poll == '3')
-        self.assertTrue(ns.magic == "Tiffany")
+        self.assertTrue(ns.dir == 'watchdir', 'Incorrectly parsing dir')
+        self.assertTrue(ns.ext == 'txt', 'Incorrectly parsing ext')
+        self.assertTrue(
+            ns.poll == '3', 'Incorrectly parsing given polling time')
+        self.assertTrue(ns.magic == 'Tiffany',
+                        'Incorrectly parsing magic string')
+
+        """Check if the default polling time comes back as 1"""
+
+        test_args = ['watchdir', 'txt', 'Tiffany']
+        ns = p.parse_args(test_args)
+        self.assertTrue(
+            ns.poll == '1', "Default polling time should be 1 second")
+
+    # def test_exit_sigint():
